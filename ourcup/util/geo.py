ffb5c13d080f9d61510bf60d68a16b3a1d14c15a
Rahul B <rahulbot@gmail.com>
2018-06-19 11:50:26 -0400

    Update reverse geocoding API testing

--- ourcup/util/geo.py
+++ ourcup/util/geo.py
@@ -6,15 +6,15 @@ import ourcup.util.filecache as filecache
 
 logger = logging.getLogger(__name__)
 
-
-def reverse_geocode(lat,lng):
-    '''
-    Cache these results to reduce load on their server, and hopefully stay under the use limit radar
-    '''
+'''
+Cache these results to reduce load on their server, and hopefully stay under the use limit radar.
+Uses the FCC block API endpoint: https://geo.fcc.gov/api/census/#!/area/get_area
+'''
+def reverse_geocode(lat, lng):
     payload = {
         'format': 'json',
-        'latitude': round(float(lat),3),
-        'longitude': round(float(lng),3),
+        'latitude': round(float(lat), 3),
+        'longitude': round(float(lng), 3),
         'showall': True
     }
     cache_key = filecache.md5_key(str(payload['latitude'])+str(payload['longitude']))
@@ -23,7 +23,7 @@ def reverse_geocode(lat,lng):
         location = json.loads(response)
         location['cache'] = True
     else:
-        response = requests.get("http://data.fcc.gov/api/block/find", params=payload)
+        response = requests.get("https://geo.fcc.gov/api/census/block/find", params=payload)
         filecache.put(cache_key, response.content)
         location = response.json()
         location['cache'] = False
