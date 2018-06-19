25810f3f8fc12ffcb7b65420825e442cb0d78784
Rahul B <rahulbot@gmail.com>
2018-06-19 11:58:15 -0400

    updating tests to pass

--- test.py
+++ test.py
@@ -2,16 +2,19 @@
 
 import unittest
 
-from acs.test.datatest import PopulationDataManagerTest, ZipCodeDataManagerTest
-from worldcup.test.fixturestest import PickerTest, CountryCodeTranslatorTest
+from ourcup.acs.test.datatest import PopulationDataManagerTest, ZipCodeDataManagerTest
+from ourcup.fixtures.test.country_codes_test import CountryCodesTest
+from ourcup.fixtures.test.match_picker_test import PickerTest
+from ourcup.util.test.geo_test import GeoTest
 
 test_classes = [
-	PopulationDataManagerTest,
-    ZipCodeDataManagerTest, 
-	PickerTest,
-    CountryCodeTranslatorTest
+    PopulationDataManagerTest,
+    ZipCodeDataManagerTest,
+    PickerTest,
+    CountryCodesTest,
+    GeoTest
 ]
 
 for test_class in test_classes:
-	suite = unittest.TestLoader().loadTestsFromTestCase(test_class)
-	unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_class)
+    unittest.TextTestRunner(verbosity=2).run(suite)
