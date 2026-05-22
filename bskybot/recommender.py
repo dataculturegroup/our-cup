3cd2cd9c6ba53830135507ed4c1d52113f35d1db
Rahul Bhargava <rahulbot@gmail.com>
2026-05-22 10:10:25 -0400

    better time display

--- bskybot/recommender.py
+++ bskybot/recommender.py
@@ -107,7 +107,7 @@ def process_zipcode(zipcode: str) -> str:
         rec_text += "Games to watch:\n"
     for game in game_recs[:3]:  # keep it short
         local_date = _as_local_time(zipcode, game['gmt_datetime'])
-        rec_text += f"{local_date.strftime('%b %d')}: {game['Home Team']} vs {game['Away Team']} @ {local_date.strftime('%I%p')}\n"
+        rec_text += f"  {local_date.strftime('%b %d')}: {game['Home Team']} vs {game['Away Team']} @ {local_date.strftime('%-I%p')}\n"
     # random tidbit
     random_link = _random_info(country_teams[0], zipcode)
     # Bluesky caps posts at 300 graphemes. Only append the random_link if the base
